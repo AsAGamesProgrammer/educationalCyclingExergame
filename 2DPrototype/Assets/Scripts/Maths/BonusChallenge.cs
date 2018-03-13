7e8de4dea205a18a477aef53862c8cd45f0f89ea
Kristina <misskristinablinova@gmail.com>
2018-03-13 11:14:18 +0000

    Start on button set up

---
+++ 2DPrototype/Assets/Scripts/Maths/BonusChallenge.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BonusChallenge : MonoBehaviour {
+
+    public GameObject bonusPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Set panel to be visible
+    public void ShowPanel()
+    {
+        bonusPanel.SetActive(true);
+    }
+
+    //Set panel to be invisible and return to the game
+    public void HidePanel()
+    {
+        bonusPanel.SetActive(false);
+    }
+}
