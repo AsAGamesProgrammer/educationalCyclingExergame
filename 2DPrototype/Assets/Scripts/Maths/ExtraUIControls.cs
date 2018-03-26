448edee80ffcc9b4ee92f80cd091479a4bb72aa7
Kristina <misskristinablinova@gmail.com>
2018-03-26 08:54:43 +0100

    GoBackBtn linked

---
+++ 2DPrototype/Assets/Scripts/Maths/ExtraUIControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Current elements:
+/// *GO BACK button
+/// </summary>
+
+public class ExtraUIControls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnGoBackClick()
+    {
+        SceneManager.LoadScene("avatarCreation");
+    }
+}
