ccaba0c13e0f1834834263ecafe2737270650c89
Kristina <misskristinablinova@gmail.com>
2018-04-10 12:20:08 +0100

    Extra background music work

--- 2DPrototype/Assets/Scripts/Audio/MathsAudio.cs
+++ 2DPrototype/Assets/Scripts/Audio/MathsAudio.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class MathsAudio : MonoBehaviour {
 
-
+    //Audio
     public AudioSource backgrounSource;
     public AudioClip[] backgroundClips = new AudioClip[3];
+
+    //Scripts
+    AvatarProgress progressScript;
 
 	// Use this for initialization
 	void Start ()
     {
-		
-	}
+        //Find script
+        progressScript = GameObject.FindGameObjectWithTag("AvatarTransfer").GetComponent<AvatarProgress>();
+
+        ChangeBackgroundMusic(progressScript.level);
+    }
 	
 	public void ChangeBackgroundMusic(int level)
     {
