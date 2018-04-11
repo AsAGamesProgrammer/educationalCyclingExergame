022e5751d01826afd5cb307cb2daad3dc48d171a
Kristina <misskristinablinova@gmail.com>
2018-04-11 13:43:56 +0100

    Main menu bike rotates

---
+++ 2DPrototype/Assets/Scripts/MainMenu/RotationScriptMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScriptMenu : MonoBehaviour {
+
+    public MenuZones currentZone;
+
+    public GameObject rotationSphere;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Vertical input detected
+        if (Input.GetAxis("Vertical") > 0.05f)
+        {
+            rotationSphere.transform.Rotate(0, 0 *  Time.deltaTime, -Input.GetAxis("Vertical"));
+        }
+    }
+}
+
+public enum MenuZones
+{
+    Start,
+    Options,
+    Credits,
+    Exit
+}
