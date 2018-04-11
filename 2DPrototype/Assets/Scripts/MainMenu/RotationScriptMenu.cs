774437a551263b6437775b8421b4d32a328c863d
Kristina <misskristinablinova@gmail.com>
2018-04-11 15:05:10 +0100

    Zone recognition with bicycle added

--- 2DPrototype/Assets/Scripts/MainMenu/RotationScriptMenu.cs
+++ 2DPrototype/Assets/Scripts/MainMenu/RotationScriptMenu.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RotationScriptMenu : MonoBehaviour {
 
+    //Zone
     public MenuZones currentZone;
 
+    //Sphere which rotates the bike
     public GameObject rotationSphere;
 
+    //Speed f rotation
+    public float speed = 5f;
+
+    //GUI
+    public Text selectedText;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        //Set middle text to what is currently selected
+        selectedText.text = currentZone.ToString().ToUpper();
 	}
 	
 	// Update is called once per frame
@@ -19,8 +30,16 @@ public class RotationScriptMenu : MonoBehaviour {
         //Vertical input detected
         if (Input.GetAxis("Vertical") > 0.05f)
         {
-            rotationSphere.transform.Rotate(0, 0 *  Time.deltaTime, -Input.GetAxis("Vertical"));
+            rotationSphere.transform.Rotate(0, 0, -Input.GetAxis("Vertical") * Time.deltaTime * speed);
         }
+    }
+
+    //Called externally
+    public void ZoneChanged(MenuZones zone_)
+    {
+        currentZone = zone_;
+        //Set middle text to what is currently selected
+        selectedText.text = currentZone.ToString().ToUpper();
     }
 }
 
---
+++ 2DPrototype/Assets/Scripts/MainMenu/ZoneRecognition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// TARGET: Attached to the collider on the bike
+/// </summary>
+
+public class ZoneRecognition : MonoBehaviour {
+
+    public RotationScriptMenu rotationScript;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+
+    //On collision with the zone
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "MenuZone")
+        {
+            rotationScript.ZoneChanged(other.gameObject.GetComponent<ZoneInformation>().thisZone);
+        }
+    }
+}
